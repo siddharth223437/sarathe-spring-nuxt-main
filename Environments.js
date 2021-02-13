2ecf9f9036840b9a2ef4e691bbfd53004911b808
siddharth2223437 <siddharthsarathe.nmims@gmail.com>
2021-02-12 22:49:04 -0600

    first commiy

---
+++ Environments.js
@@ -0,0 +1,13 @@
+const Environments = {
+  dev : {
+    apiEndpoint: 'http://localhost:1000'
+  },
+  test: {
+    apiEndpoint: 'http://test.sarathe.edu'
+  },
+  prod: {
+    apiEndpoint: 'http://prod.sarathe.edu'
+  }
+}
+
+export {Environments}
